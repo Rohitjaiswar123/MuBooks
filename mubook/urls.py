ace289e9ab8da7e877fb45efaf4c59637a6f3b9e
Jayesh Singh <hsinghjayesh@gmail.com>
2021-04-17 09:55:57 +0530

    done

--- mubook/urls.py
+++ mubook/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('' , include('books.urls')),
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

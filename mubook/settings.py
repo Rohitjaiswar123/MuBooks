02b7c86392490f524d1b40ad7f5695dd434f5285
Jayesh Singh <hsinghjayesh@gmail.com>
2021-04-17 10:33:17 +0530

    debug

--- mubook/settings.py
+++ mubook/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-&7trcq3hyqk%q35q9qosjl!ze7q3%-1y#y=l3)^$e9$p-(!jd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mubooks.herokuapp.com']
 
@@ -127,12 +127,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_ROOT=os.path.join(BASE_DIR , 'media')
+'
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
